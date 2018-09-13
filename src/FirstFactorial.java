4c906ada4996caf86a4beb52a518f63a5211b328
mikelprincesa4 <mikelprincesa@gmail.com>
2018-09-13 06:56:42 -0400

    FirstFactorial Challenge Complete 9/9/18

--- src/FirstFactorial.java
+++ src/FirstFactorial.java
@@ -1,6 +1,8 @@
 import java.util.*; 
 import java.io.*;
 
+/* Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer. */
+
 class Main {  
   public static int FirstFactorial(int num) { 
     int res = 1;
